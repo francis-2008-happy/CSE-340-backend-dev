cee901b936a6698e80f39f309ab771cf72ce42a4
francis-2008-happy <francisbucci2@gmail.com>
2025-07-19 07:20:21 +0100

    modify

--- routes/static.js
+++ routes/static.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
+router.use(express.static(path.join(__dirname, '..', 'public')));
 
 // router.use(express.static("public"));
 // router.use("/css", express.static(__dirname + "public/css"));
